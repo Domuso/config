ffe7a4c86eaa767c81a751658b528df1184e15c2
badunk <baduncaduncan@gmail.com>
2018-03-16 21:36:56 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# domuso-configs
+Configuration management for Domuso next-gen tech stack
+
+![CircleCI Status](https://circleci.com/gh/Domuso/domuso-configs.svg?style=shield&circle-token=b9acc16e755de9410b485b2c2edc8869966af746)
+
+Using this library requires `NODE_ENV`, `AWS_SECRET_ACCESS_KEY`, and `AWS_ACCESS_KEY_ID` environment variables defined.  It utilizes AWS's SSM Param store with the NODE_ENV as the part of the hierarchy.  Usage:
+
+```
+var config = require('domuso-config')
+
+config(['mysql_connection_string','yardi_default_endpoint']).then((values) => {
+  console.log(values) // returns ['mysql://user:pass@host/db', 'https://yardi-web-service/wsdl?']
+})
+
+```
+
+Values returned are always of type `String` as per AWS convention
+
+You can also pass in a config template object:
+
+```
+var template = {
+  mysql: {
+    host: 'some/ssm/path'
+  }
+}
+
+configs(template).then( (conf) => {
+  console.log(conf) // returns {mysql:{host: 'actual value'}}
+})
+```
+
+Keys are cached in-memory for retrieval to prevent unnecessary reloads of the config
+
+Please see the [specification]
+(https://docs.google.com/document/d/1HcdwoM-b2TsW7lxCwiIjnGGpHVHvCQu4qqmYXcCqK2c/edit?usp=sharing) for more details
---
+++ index.js
@@ -0,0 +1,93 @@
+var logger = require('domuso-logs').getLogger('domuso-configs');
+var AWS = require('aws-sdk');
+AWS.config.update({region: 'us-east-1'})
+
+var cache = {}
+
+var getAllKeys = (object) => {
+  var keys = []
+  Object.keys(object).forEach((key) => {
+    if (typeof object[key] === 'string') keys.push(object[key])
+    else if (typeof object[key] === 'object')
+      keys = keys.concat(getAllKeys(object[key]))
+  })
+  return keys
+}
+
+var populateValues = (template, values) => {
+  var output = {}
+  Object.keys(template).forEach((key) => {
+    output[key] = (typeof template[key] === 'object') ? populateValues(template[key], values) : values[template[key]]
+  })
+  return output;
+}
+
+// returns with cached object if all keys are present in the cache
+// else returns null
+var retrieveCache = (params) => {
+  var cachedResult = {}
+  for (var i = 0; i < params.length; i++){
+    if (!cache[params[i]]) return null
+    
+    cachedResult[params[i]] = cache[params[i]]
+  }
+  return Promise.resolve(cachedResult)
+}
+
+/**
+  Accepts a number of input params and returns with an promise of the requested config values
+  
+  String  config.get('host')                          {host: 'value1'}
+  Array   config.get(['host','username'])             {host: 'value1',username: 'value2'} 
+  Object  config.get({host: {name: 'ssm/path'}})      {host: {name: 'value'}}
+ **/
+ 
+module.exports = (params, cache = false) => {
+  var ssm = new AWS.SSM();
+  var outputTemplate = null
+  if (!process.env.NODE_ENV) throw new Error('NODE_ENV must be supplied as an environment variable')
+  if (!Array.isArray(params) && typeof params === 'object'){
+    outputTemplate = params
+    params = getAllKeys(params)
+  }
+  if (typeof params === 'string') params = [params]
+  if (!params.length) throw new Error('params must not be empty')
+
+  logger.log('Retrieving params: ', params)
+  
+  var result = cache && retrieveCache(params)
+  if (!result){
+    result = new Promise((success, failure) => {
+      var options = {
+        Names: params.map(param => `/${process.env.NODE_ENV}/${param}`),
+        WithDecryption: true,
+      }
+      ssm.getParameters(options, (err, data) => {
+        if (err) return failure(err)
+        if (data.InvalidParameters && data.InvalidParameters.length > 0){
+          logger.log(`Invalid requested params ${data.InvalidParameters}`)
+          return failure(new Error(`Invalid requested params ${data.InvalidParameters}`)) 
+        }
+        var output = {}
+        var values = data.Parameters.reduce((prev, param) =>{
+          var normalizedKey = param.Name.substring(process.env.NODE_ENV.length + 2) // strip the leading / as well
+          prev[normalizedKey] = param.Value
+          return prev
+        }, output)
+        
+        Object.assign(cache, values) // save to cache
+        
+        logger.log(`Received params: ${output}`)
+        success(values)
+      })
+    });
+  }
+  return result.then(values => {
+    var output = values 
+    if (outputTemplate){
+      logger.log('sending params', values)
+      output = populateValues(outputTemplate, values)
+    }
+    return output
+  })
+}
