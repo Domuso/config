eae571b867a7cc3209adbbb532034ba3b262fdd0
ron23 <ron@domuso.com>
2018-12-05 14:34:59 -0800

    skip fetching values if file exists and user chose not to overwrite the file. also better logging.

--- bin/builder.js
+++ bin/builder.js
@@ -29,58 +29,59 @@ outputPath = path.join(process.cwd(), outputPath || DEFAULT_OUTPUT_PATH);
 
 if (showWarning) {
   console.warn(`\x1b[34mtemplatePath and OutputPath we're not passed. Using default values:\
-    \n\nTemplatePath: ${templatePath}\n\nOutputPath: ${outputPath} \
+    \n\nTemplatePath: ${templatePath}\n\nOutputPath: ${outputPath}\n\nenvironnement: ${
+    process.env.NODE_ENV
+  } \
     \n\nYou can also use the following format: \x1b[33mbuilder template=<path> output=<path>\x1b[0m\n`);
 }
 
 console.log("\x1b[32mSTARTING TO BUILD CONFIG FILE\x1b[0m\n");
-const configTmpl = require(templatePath);
 
-console.log("Getting Config from SSM");
-config
-  .get(configTmpl)
-  .catch(e => {
-    console.log("\x1b[31m\nERROR: Failed Getting SSM config\n\x1b[0m");
-    console.log(`\nDetails: ${e}`);
+if (fs.existsSync(outputPath)) {
+  const readline = require("readline").createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+  readline.question(`\x1b[41mConfig file already exists. Overwrite (y/n)?\x1b[0m `, answer => {
+    if (["yes", "y"].includes(answer.toLowerCase())) {
+      writeFile();
+    } else {
+      console.log("Skipping config creation");
+    }
+    readline.close();
+  });
+} else {
+  writeFile();
+}
+
+function writeFile() {
+  console.log("Getting Config from SSM");
+  let configTmpl;
+  try {
+    configTmpl = require(templatePath);
+  } catch (e) {
+    console.error("\x1b[31m\nERROR: Config template file not found\n\x1b[0m");
+    console.error(e);
     process.exit(1);
-  })
-  .then(cfg => {
-    console.log("\x1b[32m\nSuccessfully got SSM config\n\x1b[0m");
+  }
 
-    function writeFile() {
+  config
+    .get(configTmpl)
+    .catch(e => {
+      console.error("\x1b[31m\nERROR: Failed Getting SSM config\n\x1b[0m");
+      console.error(e);
+      process.exit(1);
+    })
+    .then(cfg => {
+      console.log("\x1b[32m\nSuccessfully got SSM config\n\x1b[0m");
       console.log(`Writing to ${outputPath}`);
       fs.writeFileSync(outputPath, "module.exports =" + JSON.stringify(cfg)); // { flag: "w" } => default. overwrites. { flag: "wx" } => don't overwrite
-      console.log("Done Building config");
-    }
-
-    let shouldWrite = true;
-    if (fs.existsSync(outputPath)) {
-      const readline = require("readline").createInterface({
-        input: process.stdin,
-        output: process.stdout
-      });
-
-      return readline.question(
-        `\x1b[41mConfig file already exists. Overwrite (y/n)?\x1b[0m `,
-        answer => {
-          if (!["yes", "y"].includes(answer.toLowerCase())) {
-            console.log("Skipping config creation");
-            shouldWrite = false;
-          }
-
-          if (shouldWrite) {
-            writeFile();
-          }
-
-          readline.close();
-        }
-      );
-    } else {
-      writeFile();
-    }
-  })
-  .catch(e => {
-    console.log("\x1b[31m\nERROR: Failed creating config file\n\x1b[0m");
-    console.log(`\nDetails: ${e}`);
-    process.exit(1);
-  });
+      console.log("\x1b[32m\nDone Building config\n\x1b[0m");
+    })
+    .catch(e => {
+      console.error("\x1b[31m\nERROR: Failed writing config file\n\x1b[0m");
+      console.error(e);
+      process.exit(1);
+    });
+}
